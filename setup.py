745b627f0877a1e55091ed219bd87c81a507f189
Rohit Jain <32898962+Rohit2706@users.noreply.github.com>
2019-07-28 15:41:01 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="EBDM-Rohit2706",
-    version="0.0.1",
-    author="Rohit Jain",
-    author_email="rohitjain2706@gmail.com",
-    description="Entropy Based Distance Metric",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/rohitjain2706/EBDM",
-    packages=setuptools.find_packages(),
+    name = "EBDM-Rohit2706",
+    version = "1.0.0",
+    author = "Rohit Jain",
+    author_email = "rohitjain2706@gmail.com",
+    description = "Entropy Based Distance Metric",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/Rohit2706/EBDM",
+    packages = setuptools.find_packages(),
+    keywords = "entropy questionnnaire distance",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
