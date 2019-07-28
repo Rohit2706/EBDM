08bba40eca3d925b13f5b4f56574c1d6818aad91
Rohit Jain <32898962+Rohit2706@users.noreply.github.com>
2019-07-28 19:37:13 +0530

    Added Docstrings - Incomplete

--- code/features.py
+++ code/features.py
@@ -1,7 +1,33 @@
 class Features:
-    def __init__(self, _name, _feat_type):
-        self.name = _name
-        self.feat_type = _feat_type
+    """
+    A class used to represent a feature. A feature represennts question asked in the
+    questionnaire.
+    ...
+    Attributes
+    ----------
+    name : str
+        the name of the feature
+    feat_type : str
+        the type of feature: ordinal or nominal
+    num_cat : int
+        the number of categories for a feature in the questionnaire
+    categories : dictionary
+        a dictionary with keys as category name and values as objects of Categories class
+    entropy : float
+        the entropy of a feature in a particular population, given by
+        entropy of feature = sum(entropy of categories)
+    std_entropy : float
+        the standard entropy of a feature given by log(num_cat)
+    reliability : float
+        the reliability of a feature given by entropy/ std_entropy
+    weight : float
+        the weight of a feature given by reliability/ total_reliability
+        
+    """
+    
+    def __init__(self, name, feat_type):
+        self.name = name
+        self.feat_type = feat_type
         self.num_cat = 0
         self.categories = dict()
         self.entropy = 0
