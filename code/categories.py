6e377d6b29dfad79a3f35efb06f605410d246e7e
Rohit Jain <32898962+Rohit2706@users.noreply.github.com>
2019-07-28 19:13:27 +0530

    Update categories.py

--- code/categories.py
+++ code/categories.py
@@ -1,7 +1,37 @@
 class Categories:
- 
-    def __init__(self, _name, _index):
-        self.name = _name
-        self.index = _index
+    """
+    A class used to represent a category of a feature. A category represents a 
+    choice given in a question.
+
+    ...
+
+    Attributes
+    ----------
+    name : str
+        the name of the category
+    index : int
+        the index is used to identify the categories. It preserves the order of the 
+        categories in case of ordinal features.
+    prob : float
+        the occurence probability of a category of a feature in a particular population
+    entropy : int
+        the entropy of a category of a feature in a particular population given by
+        entropy = -p * log(p) where p is the occurence probability
+        
+    """
+  
+    def __init__(self, name, index):
+        """
+        Parameters
+        ----------
+        name : str
+            the name of the category
+        index : int
+            the index is used to identify the categories. It preserves the order of the 
+            categories in case of ordinal features.
+        """
+
+        self.name = name
+        self.index = index
         self.prob = 0
         self.entopy = 0
