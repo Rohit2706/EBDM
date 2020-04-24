4f1770f357f55a7f625c8e9a1a306a43dd738c1e
Rohit2706 <rohitjain@DESKTOP-CI8NESH.localdomain>
2020-04-24 18:14:19 +0530

    Undo misguided commit

--- Downloads/STUDY MATERIAL FOR FIRST YEAR/Second Semester/CS F111 - Computer Programming/EXTRA/CS F111/Local VariablesGlobal Variables.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int a=1;
-int main()
-{
-int a=2;
-printf("The value of a is %d",a);
-return 0;
-}
--- Downloads/Second Semester/CS F111 - Computer Programming/EXTRA/CS F111/n.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main()
-{
-printf("Welcome \n to the world of \n COMPUTER SCIENCE");
-return 0;
-}
